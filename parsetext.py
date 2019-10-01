df62369ffcddf20e227f1820cf054102ab2e94c6
alexnewman312 <39803936+alexnewman312@users.noreply.github.com>
2019-10-01 12:49:24 +0530

    Fix docstrings

--- parsetext.py
+++ parsetext.py
@@ -9,13 +9,13 @@ __init__ - main driver program
 def parse_unseen(s):
     allow='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    #add full stop in remove?
+    # Add full stop in remove?
     remove='!@#$%^&*()_\'-+=\..[{]}\||;:",<>/?/'
 
-    #Read in to string here via file handling or twitter later
-    #s='Hello, my name is Aditya, and this is a sentence! (and part of a test case...)'
-    #s='D18: Practiced the implementation of the KMP algorithm, spent time on dynamic programming algorithms, brushed up on Kruskal\'s MST algorithm and posted an article explaining Moore\'s voting algorithm. Read the basics of finding the LCA between two nodes in a tree. #100DaysOfCode'
-    #s='D17: Read up on the KMP linear time string matching algorithm. Learnt Moore\'s voting algorithm. Wrote another blog post. Finished up the previous post on union-find. Revised C++ STL. And now working on graph theory. Continued coursera\'s #MachineLearning course. #100DaysOfCode'
+    # Read in to string here via file handling or twitter later
+    # s='Hello, my name is Aditya, and this is a sentence! (and part of a test case...)'
+    # s='D18: Practiced the implementation of the KMP algorithm, spent time on dynamic programming algorithms, brushed up on Kruskal\'s MST algorithm and posted an article explaining Moore\'s voting algorithm. Read the basics of finding the LCA between two nodes in a tree. #100DaysOfCode'
+    # s='D17: Read up on the KMP linear time string matching algorithm. Learnt Moore\'s voting algorithm. Wrote another blog post. Finished up the previous post on union-find. Revised C++ STL. And now working on graph theory. Continued coursera\'s #MachineLearning course. #100DaysOfCode'
 
     buf=''
     words=[]
@@ -36,13 +36,13 @@ def parse_unseen(s):
 def parse_cumulative(tokens):
     allow='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    #add full stop in remove?
+    # Add full stop in remove?
     remove='!@#$%^&*()_\'\..-+=[{]}\||;:",<>/?/'
 
-    #Read in to string here via file handling or twitter later
+    # Read in to string here via file handling or twitter later
     s='Hello, my name is Aditya, and this is a sentence! (and part of a test case...)'
-    #s='D18: Practiced the implementation of the KMP algorithm, spent time on dynamic programming algorithms, brushed up on Kruskal\'s MST algorithm and posted an article explaining Moore\'s voting algorithm. Read the basics of finding the LCA between two nodes in a tree. #100DaysOfCode'
-    #s='D17: Read up on the KMP linear time string matching algorithm. Learnt Moore\'s voting algorithm. Wrote another blog post. Finished up the previous post on union-find. Revised C++ STL. And now working on graph theory. Continued coursera\'s #MachineLearning course. #100DaysOfCode'
+    # s='D18: Practiced the implementation of the KMP algorithm, spent time on dynamic programming algorithms, brushed up on Kruskal\'s MST algorithm and posted an article explaining Moore\'s voting algorithm. Read the basics of finding the LCA between two nodes in a tree. #100DaysOfCode'
+    # s='D17: Read up on the KMP linear time string matching algorithm. Learnt Moore\'s voting algorithm. Wrote another blog post. Finished up the previous post on union-find. Revised C++ STL. And now working on graph theory. Continued coursera\'s #MachineLearning course. #100DaysOfCode'
 
     buf=''
     s=list(s)
